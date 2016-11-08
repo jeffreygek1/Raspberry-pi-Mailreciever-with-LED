7b4557c84a0f612bfd249bcf9b70f3f596952278
jeffrey <jeffrey.gek@hotmail.com>
2016-11-08 14:14:12 +0100

    iets

--- Pi_LED_mail.py
+++ Pi_LED_mail.py
@@ -26,6 +26,11 @@ def loop():
 
     print
     "You have", countemails, "new emails!"
+    def Mailrecieved():
+        if countemails == 0:
+            return 0
+        else:
+            return 1
 
     if countemails > NEWMAIL_OFFSET:
         while True:
@@ -33,6 +38,8 @@ def loop():
             time.sleep(0.15)
             GPIO.output(GREEN_LED, False)
             time.sleep(0.15)
+            if Mailrecieved() == 0:
+                break
     else:
         GPIO.output(GREEN_LED, False)
         GPIO.output(RED_LED, True)
