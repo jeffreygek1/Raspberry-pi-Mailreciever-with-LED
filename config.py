1c2cfd14cb67fa83174c530a29c3824f084a424c
Stefan <stefanderaadt@gmail.com>
2016-11-08 13:48:29 +0100

    Geluiden NS

---
+++ config.py
@@ -0,0 +1,10 @@
+HOSTNAME = 'imap.gmail.com'
+USERNAME = 'hogeschoolultrechtmail'
+PASSWORD = 'raspberry'
+MAILBOX = 'Inbox'
+
+NEWMAIL_OFFSET = 0
+MAIL_CHECK_FREQ = 15
+
+GREEN_LED = 18
+RED_LED = 23
--- mail.py
+++ mail.py
@@ -1,7 +1,48 @@
 import pygame
+from imapclient import IMAPClient
+from config import *
+import time
+import RPi.GPIO as GPIO
 
-pygame.mixer.init()
-pygame.mixer.music.load("sounds/ns.mp3")
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy() == True:
-    continue
+# !/usr/bin/env python
+
+DEBUG = True
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GREEN_LED, GPIO.OUT)
+GPIO.setup(RED_LED, GPIO.OUT)
+
+def loop():
+    server = IMAPClient(HOSTNAME, use_uid=True, ssl=True)
+    server.login(USERNAME, PASSWORD)
+
+    print('Logging in as ' + USERNAME)
+    select_info = server.select_folder(MAILBOX)
+    print('%d messages in INBOX' % select_info['EXISTS'])
+
+    folder_status = server.folder_status(MAILBOX, 'UNSEEN')
+    countemails = int(folder_status['UNSEEN'])
+
+    print
+    "You have", countemails, "new emails!"
+
+    if countemails > NEWMAIL_OFFSET:
+        GPIO.output(GREEN_LED, True)
+        GPIO.output(RED_LED, False)
+    else:
+        GPIO.output(GREEN_LED, False)
+        GPIO.output(RED_LED, True)
+
+    time.sleep(MAIL_CHECK_FREQ)
+
+
+
+print
+'Press Ctrl-C to quit.'
+
+while True:
+    loop()
+
+GPIO.cleanup()
+
