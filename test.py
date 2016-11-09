eca0629f18df73490d0d5adcfbb9b1dbb6d65dc9
Janhub <jan.dekkers@student.hu.nl>
2016-11-09 15:46:17 +0100

    CSVschrijven en lezen toegevoegd aan test.py - Dictionary conditie werkt niet

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
-import imaplib
+# import imaplib
+import csv
 
-M = imaplib.IMAP4_SSL("imap.gmail.com")
-M.login("hogeschoolutrechtmail@gmail.com", "raspberry")
-M.select()
-typ, data = M.search(None, 'ALL')
-for num in data[0].split():
-    typ, data = M.fetch(num, '(RFC822)')
-    print ('Message %s\n%s\n' % (num, data[0][1]))
-M.close()
-M.logout()
+# M = imaplib.IMAP4_SSL("imap.gmail.com")
+# M.login("hogeschoolutrechtmail@gmail.com", "raspberry")
+# M.select()
+# typ, data = M.search(None, 'ALL')
+# for num in data[0].split():
+#     typ, data = M.fetch(num, '(RFC822)')
+#     print ('Message %s\n%s\n' % (num, data[0][1]))
+# M.close()
+# M.logout()
 
 # lees de header van de afzender (Stefan)
 
@@ -20,22 +21,32 @@ def leesHeader():
 
 leesHeader()
 
+# functie voor het schrijven naar het CSV bestand. Wordt later aangeroepen
+def schrijven(CSVbestand, afzender, tijd, messageID):
+    with open(CSVbestand, 'a', newline='') as CSVbestand:
+        CSVSchrijven = csv.writer(CSVbestand, delimiter=',')
+        CSVSchrijven.writerow((afzender, tijd, messageID))
+
+    return
+
 def CSVschrijven(CSVbestand, afzender, tijd, messageID):
 
-    # de message ID is hetzelfde
-    if not messageID in CSVbestand:
-        with open(CSVbestand, 'a', newline='') as CSVbestand:
-            CSVSchrijven = csv.writer(CSVbestand, delimiter=';')
-            CSVSchrijven.writerow((afzender, tijd, messageID))
+    # leest het csv bestand en schrijft in het CSV bestand
+    with open(CSVbestand, 'r', newline='') as CSVbestand:
+        CSVlezen = csv.DictReader(CSVbestand)
+        for regel in CSVlezen:
+            values = regel.values()
+
+        if not messageID == regel.values():
+            print(messageID)
+            schrijven('data/emails.csv', afzender, tijd, messageID)
+
+
 
     return # voor de sier
 
-CSVschrijven('CSVEmails.csv', Afzender, Tijd, MessageID)
+Afzender = 'blub'
+Tijd = '124113'
+MessageID = 'uhw4n0t87gon75hmgevnob56uytwhgyu5hmoevn5tirujfcmgvy56kufo84vyc 4v6iyfcmnrvdtgf'
 
-# # Bepaalt of het een nieuwe email is
-# def CSVlezen(CSVbestand):
-#
-#     with open() as CSVbestand
-#
-#     return
-
+CSVschrijven('data/emails.csv', Afzender, Tijd, MessageID)
