0faa50a013b72f92b057d21ec982e87addab14ba
jeffrey <jeffrey.gek@hotmail.com>
2016-11-09 12:55:01 +0100

    iets

--- mail.py
+++ mail.py
@@ -53,10 +53,10 @@ def loop():
             time.sleep(0.15)
             GPIO.output(YELLOW_LED, False)
             time.sleep(0.15)
-            if GPIO.input(BUTTON_mute) == False:
+            if GPIO.input(BUTTON_mute) == True:
                 print("mute")
                 os.system("amixer set PCM -- 0%")
-            if GPIO.input(BUTTON_unmute) == False:
+            if GPIO.input(BUTTON_unmute) == True:
                 print("unmute")
                 os.system("amixer set PCM -- 100%")
             if played == 0:
