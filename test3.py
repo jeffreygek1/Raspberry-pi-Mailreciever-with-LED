8d58c8f4d52641e44d4609daf5dadc1d21d52007
jeffrey <jeffrey.gek@hotmail.com>
2016-11-09 12:49:05 +0100

    iets

--- test3.py
+++ test3.py
@@ -6,9 +6,14 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(YELLOW_LED, GPIO.OUT)
 GPIO.setup(BUTTON_mute, GPIO.IN)
+GPIO.setup(BUTTON_unmute, GPIO.IN)
 
 while True:
 	if GPIO.input(BUTTON_mute):
 		GPIO.output(YELLOW_LED,True)
 	else :
 		GPIO.output(YELLOW_LED,False)
+    if GPIO.input(BUTTON_unmute):
+        GPIO.output(YELLOW_LED, True)
+    else:
+        GPIO.output(YELLOW_LED, False)
